8c209eaf5f07455c546e2e7b02cc9821790f2229
Thuyen Trinh <trinhngocthuyen@gmail.com>
2025-03-06 13:33:20 +0700

    CLI: Add argument to specify strategy (auto/spm/index)

--- Sources/iTestScanner/Public/ScanTests.swift
+++ Sources/iTestScanner/Public/ScanTests.swift
@@ -1,9 +1,13 @@
 public func scanTests(
   in projectRoot: String,
   derivedDataPath: String?,
-  quiet: Bool = false
+  quiet: Bool = false,
+  strategy: String? = nil
 ) throws -> [String] {
   logger.config.quiet = quiet
   logger.debug("Scanning tests for project: \(projectRoot)...\n")
-  return try TestScanner(projectRoot: projectRoot, derivedDataPath: derivedDataPath).scan()
+  return try TestScanner(
+    projectRoot: projectRoot,
+    derivedDataPath: derivedDataPath
+  ).withStrategy(strategy).scan()
 }
